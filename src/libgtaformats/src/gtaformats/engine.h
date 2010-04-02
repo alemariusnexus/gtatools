346d996028d6e94701393c882aac49ad2d3f02e5
David Lerch <alemariusnexus@gmail.com>
2010-04-02 14:44:03 +0000

    Merging in older changes (mainly introduced 'engine' part to gtaformats)

---
+++ src/libgtaformats/src/gtaformats/engine.h
@@ -0,0 +1,16 @@
+/*
+ * engine.h
+ *
+ *  Created on: 14.03.2010
+ *      Author: alemariusnexus
+ */
+
+#ifndef ENGINE_H_
+#define ENGINE_H_
+
+#include "engine/DefaultTextureProvider.h"
+#include "engine/NullTextureProvider.h"
+#include "engine/DFFRenderer.h"
+#include "engine/TextureProvider.h"
+
+#endif /* ENGINE_H_ */
