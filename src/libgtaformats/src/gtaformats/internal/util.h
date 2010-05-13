c08c80f50659d37077b438add5293e64e0efa8d9
David Lerch <alemariusnexus@gmail.com>
2010-05-13 17:25:48 +0000

    Added util.* to SVN

---
+++ src/libgtaformats/src/gtaformats/internal/util.h
@@ -0,0 +1,13 @@
+/*
+ * util.h
+ *
+ *  Created on: 24.04.2010
+ *      Author: alemariusnexus
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+void strtolower(char* dest, const char* src);
+
+#endif /* UTIL_H_ */
