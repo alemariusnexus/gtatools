6245376ce6f0f06019f1cadf62d9217f2c8a4708
alemariusnexus <alemariusnexus@1582d13f-eec6-b1d7-4d9e-349358f8d845>
2010-12-26 06:40:46 +0000

    Added some GPL headers.

--- trunk/src/libgta/src/gta/TextureIndex.cpp
+++ trunk/src/libgta/src/gta/TextureIndex.cpp
@@ -1,8 +1,20 @@
 /*
- * TextureIndex.cpp
- *
- *  Created on: 22.10.2010
- *      Author: alemariusnexus
+	Copyright 2010 David "Alemarius Nexus" Lerch
+
+	This file is part of libgta.
+
+	libgta is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	libgta is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libgta.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "TextureIndex.h"
