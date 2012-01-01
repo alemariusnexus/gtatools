02cf1e72847aa379b68c84296938f1b01c8f7f01
alemariusnexus <alemariusnexus@1582d13f-eec6-b1d7-4d9e-349358f8d845>
2012-01-01 01:49:35 +0000

    - Added some GPL headers - Changed copyright info to include the year 2012!

--- trunk/src/ResourceCompiler.cmake
+++ trunk/src/ResourceCompiler.cmake
@@ -1,3 +1,24 @@
+# Copyright 2010-2012 David "Alemarius Nexus" Lerch
+# 
+# This file is part of gtatools.
+#
+# gtatools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# gtatools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with gtatools.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Additional permissions are granted, which are listed in the file
+# GPLADDITIONS.
+
+
 # Resource Compiler
 #
 # This module provides a simple cross-platform resource compiler. It creates a C-header file for each input file containing a static array
